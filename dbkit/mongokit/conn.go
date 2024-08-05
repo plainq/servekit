47378642ee91f0c39167a870267f0c068d4c8a4d
heartwilltell <heartwilltell@gmail.com>
2024-08-05 15:34:24 +0200

    Linter

--- dbkit/mongokit/conn.go
+++ dbkit/mongokit/conn.go
@@ -47,11 +47,7 @@ func (c *Conn) Close() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := c.Disconnect(ctx); err != nil {
-		return err
-	}
-
-	return nil
+	return c.Disconnect(ctx)
 }
 
 // HealthCheck implements the health.Checker interface for MongoDB connection.
