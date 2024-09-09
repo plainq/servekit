a77e550ba552e51f54851abe2de153d6b719203c
Serhii Mariiekha <heartwilltell@gmail.com>
2024-09-09 16:33:19 +0200

    Merge pull request #8
    * Mongo options refactoring.

--- dbkit/mongokit/conn.go
+++ dbkit/mongokit/conn.go
@@ -11,23 +11,36 @@ import (
 )
 
 // Option configures the mongo options.ClientOptions.
-type Option func(*mongooptions.ClientOptions)
+type Option func(options *ConnOptions)
+
+// ConnOptions represents the configuration options
+// for the MongoDB connection Conn.
+type ConnOptions struct {
+	mongoOptions *mongooptions.ClientOptions
+	readPref     *readpref.ReadPref
+}
 
 // WithAppName sets app name to the connection properties.
 func WithAppName(name string) Option {
-	return func(o *mongooptions.ClientOptions) { o.SetAppName(name) }
+	return func(o *ConnOptions) { o.mongoOptions.SetAppName(name) }
 }
 
 // WithDirectConnection sets ability to make direct connection.
 func WithDirectConnection(direct bool) Option {
-	return func(o *mongooptions.ClientOptions) { o.SetDirect(direct) }
+	return func(o *ConnOptions) { o.mongoOptions.SetDirect(direct) }
 }
 
 // WithConnectTimeout specifies a timeout that is used for creating connections to the server. This can be set through
 // ApplyURI with the "connectTimeoutMS" (e.g "connectTimeoutMS=30") option. If set to 0, no timeout will be used. The
 // default is 30 seconds.
 func WithConnectTimeout(timeout time.Duration) Option {
-	return func(o *mongooptions.ClientOptions) { o.SetConnectTimeout(timeout) }
+	return func(o *ConnOptions) { o.mongoOptions.SetConnectTimeout(timeout) }
+}
+
+// WithReadPref sets the Mongo read preference which determines
+// which servers are considered suitable for read operations.
+func WithReadPref(readPref *readpref.ReadPref) Option {
+	return func(o *ConnOptions) { o.readPref = readPref }
 }
 
 // Conn wraps the connection to the MongoDB.
@@ -37,20 +50,24 @@ type Conn struct{ *mongo.Client }
 // Receives variadic Option to configure the MongoDB connection settings.
 func New(addr string, options ...Option) (*Conn, error) {
 	ctx := context.Background()
-	connOptions := mongooptions.Client()
 
-	for _, option := range options {
-		option(connOptions)
+	connOptions := ConnOptions{
+		mongoOptions: mongooptions.Client(),
+		readPref:     readpref.PrimaryPreferred(),
 	}
 
-	connOptions.ApplyURI(addr)
+	for _, option := range options {
+		option(&connOptions)
+	}
 
-	client, err := mongo.Connect(ctx, connOptions)
+	connOptions.mongoOptions.ApplyURI(addr)
+
+	client, err := mongo.Connect(ctx, connOptions.mongoOptions)
 	if err != nil {
 		return nil, fmt.Errorf("mongo: connection failed: %w", err)
 	}
 
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+	if err := client.Ping(ctx, connOptions.readPref); err != nil {
 		return nil, fmt.Errorf("mongo: ping failed: %w", err)
 	}
 
@@ -58,7 +75,7 @@ func New(addr string, options ...Option) (*Conn, error) {
 }
 
 func (c *Conn) Close() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	return c.Disconnect(ctx)
